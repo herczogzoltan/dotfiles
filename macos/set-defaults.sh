a9db059c0db8a516f2267248cf91968696240453
Zoltán Herczog <zoltan.herczog@pm.me>
2020-05-04 19:21:23 +0200

    Add macos-related extra configuration for finder behavior and keyboard repeat rate

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,1 +1,13 @@
+# Set the default terminal profile
 defaults write com.apple.terminal "Default Window Settings" "custom-profile"
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
