145e67a7428f5f668417f2891abbf4ab0e899e3a
Zoltán Herczog <zoltan.herczog@pm.me>
2020-05-10 15:58:13 +0200

    Separate homebrew related configuration from global config

---
+++ homebrew/homebrew.zsh
@@ -0,0 +1,2 @@
+# Do not send analytics data to Homebrew - https://docs.brew.sh/Analytics
+export HOMEBREW_NO_ANALYTICS=1
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,6 +9,3 @@ PATH=$PATH:$HOME/bin
 
 # Add Python 3 libraries
 PATH=$PATH:$HOME/Library/Python/3.7/bin
-
-# Do not send analytics data to Homebrew - https://docs.brew.sh/Analytics
-export HOMEBREW_NO_ANALYTICS=1
