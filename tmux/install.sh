424c7f9d7aad68fb84ee83dc8beffa43f12f0e44
Zoltán Herczog <zoltan.herczog@pm.me>
2020-05-10 16:02:43 +0200

    Add tmux installer and default configuration

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,18 +2,28 @@
 #
 # tmux
 
-if test ! $(which tmux)
-then
+if test ! $(which tmux); then
   echo "  Installing tmux for you."
 
   # Install homebrew if missing
-  if test ! $(which brew)
-  then
+  if test ! $(which brew); then
     echo "Homebrew missing, trying to install it."
     ./homebrew/install.sh
   fi
-brew install tmux
 
 fi
+
+# install tmux via brew
+brew install tmux
+
+if [[ -e "$HOME/.tmux.conf" ]]; then
+  printf "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
+  cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.backup" 2>/dev/null || true
+fi
+
+ln -sf $(pwd)/tmux/tmux.conf "$HOME"/.tmux.conf;
+
+# Set custom tmux configuration
+tmux source-file ~/.tmux.conf
 
 exit 0
