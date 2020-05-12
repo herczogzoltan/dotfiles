cde5d56ab27bd8a11c45b05d5e4afefbebc2ad7c
Zoltán Herczog <zoltan.herczog@pm.me>
2020-05-13 00:17:53 +0200

    Add docker-sync install

---
+++ docker-sync/config.zsh
@@ -0,0 +1,5 @@
+export PATH="/usr/local/opt/sqlite/bin:$PATH"
+
+if which ruby >/dev/null && which gem >/dev/null; then
+  PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+fi
---
+++ docker-sync/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# docker-sync
+
+if test ! $(which docker-sync); then
+  echo "  Installing docker-sync for you."
+
+  # Install homebrew if missing
+  if test ! $(which brew); then
+    echo "Homebrew missing, trying to install it."
+    ./homebrew/install.sh
+  fi
+
+fi
+
+# install docker-sync and dependencies via brew
+brew install unison
+brew install eugenmayer/dockersync/unox
+if which ruby >/dev/null && which gem >/dev/null; then
+  gem install --user-install docker-sync
+  exit 0
+fi
+
+
+exit 1
